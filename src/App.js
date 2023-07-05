4dfcf5ae5298aaa5c554a4476ec40a515607e68c
Maitri <maitrip007@gmail.com>
2023-07-05 19:09:00 +0530

    update

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ setToDo(newState);
   return (
     <>
     <div className="bg-black h-screen p-3 flex items-center justify center ">
-      <div className="bg-[#8EC5FC] mx-auto rounded w-[600px] h-[450px]">
+      <div className="bg-white mx-auto rounded w-[600px] h-[450px]">
         <Input handler={addToDoHandler}/>
         <List data={todo} removeHandler={removeToDo}/>
       </div>
